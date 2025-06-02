f830c205f1b2d26c070eb6a98694c284794fdd2c
Shadrack <shadrackmeshack7@gmail.com>
2025-06-03 00:09:40 +0300

    Advanced Queries

--- week-1-mongodb-fundamentals-assignment-shadrack-ago-main/queries.js
+++ week-1-mongodb-fundamentals-assignment-shadrack-ago-main/queries.js
@@ -14,3 +14,29 @@ db.books.updateOne(
 
 // 5. Delete a book by its title
 db.books.deleteOne({ title: "Moby Dick" })
+
+
+// Task 3: Advanced Queries
+
+// 1. Find books in stock AND published after 2010
+db.books.find({ 
+  in_stock: true,
+  published_year: { $gt: 2010 }
+})
+
+// 2. Projection (only title, author, price)
+db.books.find(
+  { genre: "Fantasy" },
+  { title: 1, author: 1, price: 1, _id: 0 }
+)
+
+// 3. Sorting
+// Ascending by price
+db.books.find().sort({ price: 1 })
+// Descending by price
+db.books.find().sort({ price: -1 })
+
+// 4. Pagination (5 books per page, 2nd page)
+db.books.find()
+  .skip(5)  // Skips first 5 (page 1)
+  .limit(5) // Shows next 5 (page 2)
