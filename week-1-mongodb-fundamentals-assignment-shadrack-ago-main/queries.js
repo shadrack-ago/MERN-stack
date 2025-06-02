b85ae8e36a2e790f730bb4fbfa349fad6d8188f2
Shadrack <shadrackmeshack7@gmail.com>
2025-06-03 00:01:50 +0300

    Delete a book by its title

--- week-1-mongodb-fundamentals-assignment-shadrack-ago-main/queries.js
+++ week-1-mongodb-fundamentals-assignment-shadrack-ago-main/queries.js
@@ -5,3 +5,12 @@ db.books.find({ published_year: { $gt: 1950 } })
 
 // 3. Find books by a specific author
 db.books.find({ author: "George Orwell" })
+
+// 4. Update the price of a specific book
+db.books.updateOne(
+  { title: "The Hobbit" },
+  { $set: { price: 15.99 } }
+)
+
+// 5. Delete a book by its title
+db.books.deleteOne({ title: "Moby Dick" })
