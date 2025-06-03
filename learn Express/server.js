2815fb55d9e27931eaa854fe2158d9bf1885a945
Shadrack <shadrackmeshack7@gmail.com>
2025-06-03 16:15:45 +0300

    created my first webserver

--- learn Express/server.js
+++ learn Express/server.js
@@ -4,8 +4,7 @@ app.set('view engine', 'ejs');
 //creating routes
 app.get('/',(req,res)=>{
     console.log('Request received');           
-    res.status(200).json({message: "you have an error"});
-    res.render('index.html');
+    res.render('index');
 
 })
 app.listen(3000)
