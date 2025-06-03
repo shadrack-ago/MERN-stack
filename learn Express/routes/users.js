d19399d70fa1676c67563541f11fcd06c31b8c94
Shadrack <shadrackmeshack7@gmail.com>
2025-06-03 22:51:49 +0300

    routes using middlewares

---
+++ learn Express/routes/users.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const router=express.Router();
+
+//creating routes
+router.get('/',(req,res)=>{
+    console.log('Request received');           
+    res.render('index');
+})
+
+router.get('/new',(req,res)=>{
+    console.log('Request received');           
+    res.render().json({message: 'New User Page'});
+})
