c1e2bdaead722c9c6eb7dbf6d3398214fdd93f14
Shadrack <shadrackmeshack7@gmail.com>
2025-06-03 15:48:18 +0300

    view engine using ejs

--- learn Express/server.js
+++ learn Express/server.js
@@ -1,8 +1,11 @@
 const express=require('express');
 const app=express();
+app.set('view engine', 'ejs');
+//creating routes
 app.get('/',(req,res)=>{
     console.log('Request received');           
-    res.send('Hello World!');
+    res.status(200).json({message: "you have an error"});
+    res.render('index.html');
 
 })
 app.listen(3000)
