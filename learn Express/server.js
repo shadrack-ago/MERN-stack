1a9ad0e14181461b98168df772a283644c9eb212
Shadrack <shadrackmeshack7@gmail.com>
2025-06-03 15:04:36 +0300

    updates

--- learn Express/server.js
+++ learn Express/server.js
@@ -1,1 +1,8 @@
-console.log("Starting server... bye");
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    console.log('Request received');           
+    res.send('Hello World!');
+
+})
+app.listen(3000)
