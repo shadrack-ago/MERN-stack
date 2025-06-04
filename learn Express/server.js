60d56228aa4b75d110a5b1308caa16ceef612a41
Shadrack <shadrackmeshack7@gmail.com>
2025-06-04 10:50:46 +0300

    added port number

--- learn Express/server.js
+++ learn Express/server.js
@@ -1,10 +1,16 @@
 const express=require('express');
 const app=express();
+const port=3000;
 app.set('view engine', 'ejs');
 //creating routes
-app.get('/',(req,res)=>{
+app.get('/',(req,res,)=>{
     console.log('Request received');           
     res.render('index');
 
 })
-app.listen(3000)
+
+const usersRouter=require('./routes/users');
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)});
